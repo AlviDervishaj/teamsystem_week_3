acefe55841da627e6eac11d6ec301b0e6f3fb1a3
Alvi Dervishaj <alvidervishaj9@gmail.com>
2024-03-06 11:45:42 +0100

    Added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,20 @@
 import { TracingBeam, LampEffect, About, Projects } from "./components/ui";
 import { ReactLenis } from '@studio-freight/react-lenis';
 import { SpeedInsights } from "@vercel/speed-insights/react"
+import { Analytics } from "@vercel/analytics/react"
 import "./output.css";
 
 function App() {
   return (
     <ReactLenis root>
       <TracingBeam>
-        <LampEffect title={"Alvi Dervishaj"} description={"Front End Developer"} />
+        <LampEffect title={"Alvi Dervishaj"} description={"Front End Deper"} />
         <div className="h-40 w-full md:relative hidden" />
         <About />
         <Projects />
       </TracingBeam>
       <SpeedInsights />
+      <Analytics />
     </ReactLenis>
   );
 }
