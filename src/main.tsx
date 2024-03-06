2bbf69e8ccedf03f39d0ac0feb9455d8db3119ff
Alvi Dervishaj <alvidervishaj9@gmail.com>
2024-03-06 11:25:06 +0100

    Changed speed insight from next to react

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
