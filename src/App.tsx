9236eed83d6eaa5aa7bca84648a975fcd94ea35b
Alvi Dervishaj <alvidervishaj9@gmail.com>
2024-03-07 09:11:06 +0100

    Changed Header text because it was missing a word.

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <ReactLenis root>
       <TracingBeam>
-        <LampEffect title={"Alvi Dervishaj"} description={"Front End Deper"} />
+        <LampEffect title={"Alvi Dervishaj"} description={"Front End Developer"} />
         <div className="h-40 w-full md:relative hidden" />
         <About />
         <Projects />
